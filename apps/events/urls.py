aba6d48cad10b5f83575c93ae2662f7537125d7a
Yonsy Solis <yonsy.solis@gmail.com>
2011-04-26 16:48:00 -0500

    reports finished

--- apps/events/urls.py
+++ apps/events/urls.py
@@ -3,12 +3,15 @@ from django.contrib.auth.decorators import login_required
 
 from surlex.dj import surl
 
-from views import EventListView, EventDetailView
+from views import EventListView, EventDetailView, ReportListView, ReportDetailView
+
 from views import event_register
 
 urlpatterns = patterns('',
     url(r'^$', login_required(EventListView.as_view()), name='event_list'),
     surl(r'^event/<pk:s>/$', login_required(EventDetailView.as_view()), name='event_detail'),
     surl(r'^event/<pk:s>/register', login_required(event_register), name='event_register'),
+    surl(r'^report/$', ReportListView.as_view(), name='report_list'),
+    surl(r'^report/<pk:s>/$', ReportDetailView.as_view(), name='report_detail'),
 )
 
--- apps/events/views.py
+++ apps/events/views.py
@@ -5,6 +5,7 @@ from django.utils.safestring import mark_safe
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.shortcuts import get_object_or_404
+from django.contrib.auth.decorators import permission_required
 from django.conf import settings
 
 from models import Event
@@ -12,6 +13,11 @@ from models import Event
 
 class EventListView(ListView):
     model = Event
+
+    def get_context_data(self, **kwargs):
+        context = super(EventListView, self).get_context_data(**kwargs)
+        context['can_view_report'] = self.request.user.has_perm('events.can_view_report')
+        return context
 
 
 class EventDetailView(DetailView):
@@ -25,11 +31,31 @@ class EventDetailView(DetailView):
 
         event = self.object
         user = self.request.user
-
         context['user_suscribed'] = event.suscribed(user)
 
         return context
 
+
+class ReportListView(ListView):
+    model = Event
+    template_name = 'events/report_list.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        @permission_required('events.can_view_report')
+        def wrapper(request, *args, **kwargs):
+            return super(ReportListView, self).dispatch(request, *args, **kwargs)
+        return wrapper(request, *args, **kwargs)
+
+
+class ReportDetailView(DetailView):
+    model = Event
+    template_name = 'events/report_detail.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        @permission_required('events.can_view_report')
+        def wrapper(request, *args, **kwargs):
+            return super(ReportDetailView, self).dispatch(request, *args, **kwargs)
+        return wrapper(request, *args, **kwargs)
 
 
 def event_register(request, pk):
@@ -43,3 +69,5 @@ def event_register(request, pk):
         event.save()
 
     return HttpResponseRedirect(reverse('event_detail', args=[pk]))
+
+
